2c80754928700f4dae5d262869e442cdd1419788
Bug1642 <hasancan1642@hotmail.com>
2024-12-23 15:38:12 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace LengthExercise {
+    public class Strings {
+        public bool doesLengthMatch(string source, int count) {
+           bool lengthMatches = source.Length == count;
+            return lengthMatches;
+        }
+    }
+    class Program{
+        static void Main( string [] args ){
+            Strings match = new Strings();
+            Console.WriteLine(match.doesLengthMatch("a",1));
+            Console.WriteLine(match.doesLengthMatch("abc",1));
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Manipulation2
+ String Manipulation Activity Part II
